b881b6d69df863923439715eb043442cdd7611e9
ThepsirinN <thepsirin.n@alumni.chula.ac.th>
2022-12-07 16:04:17 +0700

    chroe: add Sweetalert and axious make register post to api and database;012_Thepsirin

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import axios from "axios";
+import Swal from 'sweetalert2'
 import { Link } from "react-router-dom";
 import styles from "./register.module.css";
 import logo from "./img/logo-fitness-fog.png";
@@ -5,25 +8,59 @@ import { FaUser, FaLock } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+  // const [errorObj, setErrorObj] = useState({});
+
+  const register = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:4000/user/createUser", {
+        username: username,
+        email: email,
+        pass: pass,
+        confirmPass: confirmPass,
+      });
+      if(response){
+        Swal.fire({
+          title: `${response.data.msg}!`,
+          icon: 'success',
+          confirmButtonText: 'OK',
+        })
+      }
+    } catch (err) {
+      if (err.response.status === 400) {
+        Swal.fire({
+          title: `Bad Request!`,
+          text: `${err.response.data.msg}`,
+          icon: 'error',
+          confirmButtonText: 'OK',
+        })
+      }
+    }
+  };
+
   return (
     <div className={styles["Register"]}>
       <div className={styles["img-bg"]}></div>
       <div className={styles["opa"]}></div>
 
       <header className={styles["register-logo-header"]}>
-      <Link to={"../"} style={{ textDecoration: "none" }}>
-        <img src={logo} width="63px" alt="fitness-frog logo" />
-      </Link>
-      <Link to={"../"} style={{ textDecoration: "none" }}>
-        <h3>Fitness Frog</h3>
-      </Link>
+        <Link to={"../"} style={{ textDecoration: "none" }}>
+          <img src={logo} width="63px" alt="fitness-frog logo" />
+        </Link>
+        <Link to={"../"} style={{ textDecoration: "none" }}>
+          <h3>Fitness Frog</h3>
+        </Link>
       </header>
 
       <main className={styles["register-container"]}>
         <section className={styles["register-title"]}>
           <h1 className="text-dark">Register</h1>
         </section>
-        <form className={styles["register-Form"]}>
+        <form className={styles["register-Form"]} onSubmit={register}>
           <div className={styles["form-group"]}>
             <HiOutlineMail
               className={styles["iconShow"]}
@@ -34,6 +71,11 @@ const Register = () => {
               name="email-register"
               id="email-register-id"
               placeholder="E-mail"
+              autoComplete="off"
+              value={email}
+              onChange={({ target }) => {
+                return setEmail(target.value);
+              }}
             />
           </div>
 
@@ -47,6 +89,11 @@ const Register = () => {
               name="username-register"
               id="username-register-id"
               placeholder="Username"
+              autoComplete="off"
+              value={username}
+              onChange={({ target }) => {
+                return setUsername(target.value);
+              }}
             />
           </div>
 
@@ -60,6 +107,11 @@ const Register = () => {
               name="password-register"
               id="password-register-id"
               placeholder="Password"
+              autoComplete="off"
+              value={pass}
+              onChange={({ target }) => {
+                return setPass(target.value);
+              }}
             />
           </div>
 
@@ -73,6 +125,11 @@ const Register = () => {
               name="confirm-password-register"
               id="confirm-password-register-id"
               placeholder="Confirm Password"
+              autoComplete="off"
+              value={confirmPass}
+              onChange={({ target }) => {
+                return setConfirmPass(target.value);
+              }}
             />
           </div>
           <section className={styles["register-button"]}>

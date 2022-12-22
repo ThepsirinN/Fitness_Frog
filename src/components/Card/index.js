d8efb189983c111133b7f8177440ae6a8f863f9f
ThepsirinN <thepsirin.n@alumni.chula.ac.th>
2022-12-22 21:42:20 +0700

    finished:We M(Footer &timezone) 🎉🎉🎉 (ex upload img);All

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -22,7 +22,7 @@ const Card = (props) => {
   const cardColor = ["#25eae7","#98ff98","#eef221","#ff8c01","#f77fbe"];
   const status = ["Pending⌚", "Completed✅"];
   const startDate = new Date(props.startDate);
-  const startDateString = startDate.toLocaleString("en-US", {timeZone: "Asia/Bangkok"});
+  const startDateString = startDate.toLocaleString("en-GB", {timeZone: "UTC"});
   const endDate = new Date(props.endDate);
   /* const endDateString = endDate.toLocaleString() */
   const minuteDuration = (endDate - startDate) / (60 * 1000);
